2c8e879de4d9936c2c5d8c3e7e9bf8d1e5f27adf
Ranyu <ranyu2@126.com>
2015-07-27 14:45:12 +0800

    dbn_min update

--- dnn/dbn_min.py
+++ dnn/dbn_min.py
@@ -4,9 +4,9 @@ import numpy
 import cPickle
 from sklearn.cross_validation import train_test_split
 
-train_x = numpy.load('data/fuck_fbank_X_n.npy')
-test_x = numpy.load('data/fuck_fbank_test_n.npy')
-train_y = numpy.load('data/1943_label.npy')
+train_x = numpy.load('data/X_train.npy')
+test_x = numpy.load('data/X_test.npy')
+train_y = numpy.load('data/Y_train.npy')
 
 print train_x.shape
 print train_y.shape
@@ -15,18 +15,23 @@ print test_x.shape
 (train_x,vali_x,train_y,vali_y) = train_test_split(train_x,train_y,test_size = 0.2)
 
 dbn = DBN(
-        [759,1024,1943],
+        [300,1024,120000],
         learn_rates = 0.025,
         learn_rate_decays = 0.98,
         l2_costs = 0.0001,
         minibatch_size=256,
-        epochs=10,
+        epochs=5,
         momentum = 0.9,
         #dropouts=0.22,
         verbose = 2)
 
 dbn.fit(train_x, train_y)
 print 'validation score is:' ,dbn.score(vali_x,vali_y)
+
+result = dbn.predict(test_x)
+with open('data/result','w') as f:
+    for el in result:
+        f.write(el+'\n')
 
 #predicted_y_proba = dbn.predict_proba(test_x)
 
