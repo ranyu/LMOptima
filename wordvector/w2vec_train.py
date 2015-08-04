37c86eddabe32ae4c0c36193eb3521165cedfb98
Ranyu <ranyu2@126.com>
2015-08-04 14:59:09 +0800

    add gene text

--- wordvector/w2vec_train.py
+++ wordvector/w2vec_train.py
@@ -27,47 +27,62 @@ class w2vec:
     def similarity(self,w1,w2):
         a1 = np.array(w1)
         a2 = np.array(w2)
-        print a1,a2
-        return cosine(a1,a2)
-    
-def get_result(model):
+        #print a1,a2
+        return np.dot(a1,a2)
+def load_dic():
+    dic = []
+    with open('../core_table/asso_unigram.dic') as f:
+        f.readline()
+        f.readline()
+        for data in f:
+            dic.append(data.strip().split()[1].decode('gb18030').encode('utf-8'))
+        return dic
+
+def get_result(model,sys_dic):
     sentences = []
     #with open('../../LMOptima/rnnlm-0.4b/result/sys_result','r') as f:
-    with open('./result','r') as f:
+    with open('result/sys_result','r') as f:
         for data in f:
             sentences.append(data.strip())#.decode('gb18030').encode('utf-8'))
             #sentences.append(data.strip())
     score = 0
     scores = []
-    for param_1 in [-1.,-2.,-3.,0,1,2,3,4]:
-        for param_2 in [-1.,-2.,-3.,0,1,2,3,4]:
-            for p in sentences:
+    for param_1 in [-1.]:#[-1.,-2.,-3.,0,1,2,3,4]:
+        for param_2 in [-1.]:#[-1.,-2.,-3.,0,1,2,3,4]:
+            print param_1,param_2
+            for i,p in enumerate(sentences):
                 words = p.split()
-                for q in words:
-                    print q
-                #print words.decode('gb18030').encode('utf-8')
+                if words[1] in answer:
+                    print words[1],i,answer.index(words[1])
                 for q in words[0:-1]:
-                    print q,model.__contains__(q)
-                    print np.array(model.vector[q])
+                    print i
                     if model.__contains__(q):
-                        print words[-1]
                         if model.__contains__(words[-1]):
                             score += pow(model.vocab[q],param_1)*((model.similarity(model.vector[q],model.vector[words[-1]])+2)**param_2)
-                        print (score)
-                        raw_input()
                 scores.append(score)
+                score = 0
+            print 'calculation finish'
             for sq in heapq.nlargest(10,range(len(scores)),scores.__getitem__):
-                print (sentences[sq],scores[sq])
-                for ql in sentences[sq]:
-                    print ql
+                for an in sentences[sq]:
+                    print an
+                print sco
+                    print (sentences[sq],scores[sq])
             scores = []
-            score = 0
             raw_input()
+def gene_text(goal,fea_index):
+    with open('result/sys_result','w') as f:
+        for w in fea_index:
+            f.write(goal+' '+w.decode('gb18030').encode('utf-8')+'\n')
+
 
 def main():
     model = w2vec()
     print ('load')
-    get_result(model) 
+    sys_dic = load_dic()
+    while True:
+        goal = raw_input('Input the word:\n')
+        gene_text(goal,sys_dic)
+        get_result(model,sys_dic) 
     print ('finish')
 
 if __name__ == "__main__":
